1a1d878fb8644f3f68e4948d15eaab9be06be735
tonyreaves <tonyfreaves@gmail.com>
2018-07-02 15:54:19 -0700

    language for omdb

--- liri.js
+++ liri.js
@@ -1,41 +1,46 @@
 require("dotenv").config();
-var request = require(keys.js);
+// var keyRequest = require("keys.js");
 
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 var inquirer = require("inquirer");
+var nodeArgs = process.argv;
 
-
-var movieRequest = require("request");
 var movieThis = "";
-
-// Request module on a URL with a JSON
-request("http://www.omdbapi.com/?t=" + movieThis + "&y=&plot=short&apikey=trilogy", function(error, response, body) {
-
-  // If no errors and response code is 200 
-  if (!error && response.statusCode === 200) {
-
-    // Print out the omdb return
-    console.log("=====================");
-    console.log("Title: " + JSON.parse(body).imdbRating);
-    console.log("Year: " + JSON.parse(body).imdbRating);
-    console.log("iMDB Rating: " + JSON.parse(body).imdbRating);
-    console.log("Rotten Tomatoes Rating: " + JSON.parse(body).imdbRating);
-    console.log("Country: " + JSON.parse(body).imdbRating);
-    console.log("Language: " + JSON.parse(body).imdbRating);
-    console.log("Plot: " + JSON.parse(body).imdbRating);
-    console.log("Actors: " + JSON.parse(body).imdbRating);
-  }
-});
-
-inquirer
-  .prompt([
-    // Basic text prompt.
-    {
-      type: "input",
-      message: "What is your name?",
-      name: "username"
-    },
+var movieRequest = require("http://www.omdbapi.com/?t=" + movieThis + "&y=&plot=short&apikey=trilogy");
 
 
-])
+if (nodeArgs[2] === "movie-this") {
+    for (var i = 4; i < nodeArgs.length; i++) {
+
+        if (i > 4 && i < nodeArgs.length) {
+
+            movieThis = movieThis + "+" + nodeArgs[i]
+        }
+
+        else {
+            movieThis += nodeArgs[i];
+        }
+    }
+
+
+    // Request module on a URL with a JSON
+    movieRequest("http://www.omdbapi.com/?t=" + movieThis + "&y=&plot=short&apikey=trilogy", function (error, response, body) {
+
+        // If no errors and rxesponse code is 200 
+        if (!error && response.statusCode === 200) {
+
+            // Print out the omdb return
+            console.log("=====================");
+            console.log("Title: " + JSON.parse(body).Title);
+            console.log("Year: " + JSON.parse(body).Year);
+            console.log("iMDB Rating: " + JSON.parse(body).imdbRating);
+            console.log("Rotten Tomatoes Rating: " + JSON.parse(body).tomatoMeter);
+            console.log("Country: " + JSON.parse(body).Country);
+            console.log("Language: " + JSON.parse(body).Language);
+            console.log("Plot: " + JSON.parse(body).Plot);
+            console.log("Actors: " + JSON.parse(body).Actors);
+        }
+    });
+
+}
