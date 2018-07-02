2379187d938c1f74178f40e27225622d98377d78
tonyreaves <tonyfreaves@gmail.com>
2018-07-02 13:23:45 -0700

    got all keys in place

--- keys.js
+++ keys.js
@@ -1,11 +1,12 @@
 console.log('this is loaded');
 exports.twitter = {
-  consumer_key: process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  consumer_key: process.env.gCqQON2FTZTB2tIBRYbP6py6x,
+  consumer_secret: process.env.6cXvKcBNrisObWVGvd8CLMBT1QjWSPc0OkTNjmU9niZoBkfYh5,
+  access_token_key: process.env.1012492328936751104-Yb8K8NtxIIR0EA33C1VxsyMnPXQbHw
+  ,
+  access_token_secret: process.env.DxNaDTTgKXuVOeWM77r3FtzFgYIheTL6t6yYuBxtx5UH6
 };
 exports.spotify = {
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
+  id: process.env.9d659cb3d0e8486b9cca76cb023136a9,
+  secret: process.env.5f022c4237c8404cbf6779712aa0a150
 };
--- liri.js
+++ liri.js
@@ -3,3 +3,39 @@ var request = require(keys.js);
 
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
+var inquirer = require("inquirer");
+
+
+var movieRequest = require("request");
+var movieThis = "";
+
+// Request module on a URL with a JSON
+request("http://www.omdbapi.com/?t=" + movieThis + "&y=&plot=short&apikey=trilogy", function(error, response, body) {
+
+  // If no errors and response code is 200 
+  if (!error && response.statusCode === 200) {
+
+    // Print out the omdb return
+    console.log("=====================");
+    console.log("Title: " + JSON.parse(body).imdbRating);
+    console.log("Year: " + JSON.parse(body).imdbRating);
+    console.log("iMDB Rating: " + JSON.parse(body).imdbRating);
+    console.log("Rotten Tomatoes Rating: " + JSON.parse(body).imdbRating);
+    console.log("Country: " + JSON.parse(body).imdbRating);
+    console.log("Language: " + JSON.parse(body).imdbRating);
+    console.log("Plot: " + JSON.parse(body).imdbRating);
+    console.log("Actors: " + JSON.parse(body).imdbRating);
+  }
+});
+
+inquirer
+  .prompt([
+    // Basic text prompt.
+    {
+      type: "input",
+      message: "What is your name?",
+      name: "username"
+    },
+
+
+])
