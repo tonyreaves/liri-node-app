7df0532ca06a66fe477f87ea7540bc8e7006eeb0
tonyreaves <tonyfreaves@gmail.com>
2018-07-07 17:09:05 -0700

    this won't work

--- liri.js
+++ liri.js
@@ -9,36 +9,85 @@ var twitter = require('twitter');
 var client = new twitter(keys.twitter);
 var spotifyApi = new Spotify(keys.spotify);
 var nodeArgs = process.argv;
-var movie = "";
+var name = "";
 var doThis = "";
-var heyLiri = nodeArgs[2];
+var heyLiri = nodeArgs[2]
 
+
+//========functions==========
+
+function getName(input){
+    for (var i = 3; i < nodeArgs.length; i++) {
+
+        if (i > 3 && i < nodeArgs.length) {
+
+            var name = name + "+" + nodeArgs[i]
+        }
+
+        else {
+            name += nodeArgs[i];
+        }
+    }
+}
+
+function movieSearch(nodeArgs[3]) {
+    //If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
+    if (nodeArgs[3] === undefined) {
+        name = "Mr.+Nobody"
+    }
+
+    //looks for additional words in search term, puts + between words
+    else {
+        getName();
+    }
+
+    //the query url
+    var movieQuery = "http://www.omdbapi.com/?t=" + name + "&y=&plot=short&tomatoes=true&apikey=trilogy";
+
+    // Request module on a URL with a JSON
+    request(movieQuery, function (error, response, body) {
+        // If no errors and rxesponse code is 200 
+        if (!error && response.statusCode === 200) {
+
+            // Print out the omdb return
+            console.log("=====================");
+            console.log("Title: " + JSON.parse(body).Title);
+            console.log("Year: " + JSON.parse(body).Year);
+            console.log("iMDB Rating: " + JSON.parse(body).imdbRating);
+            console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+            console.log("Country: " + JSON.parse(body).Country);
+            console.log("Language: " + JSON.parse(body).Language);
+            console.log("Plot: " + JSON.parse(body).Plot);
+            console.log("Actors: " + JSON.parse(body).Actors);
+        }
+        else console.log("There was an error.")
+    }
+    )
+}
 //========JSON requests =====================================================
 
 if (nodeArgs[2] === "do-what-it-says") {
 
     var fs = require("fs");
-    
-    fs.readFile("random.txt", "utf8", function(error, data) {
+
+    fs.readFile("random.txt", "utf8", function (error, data) {
 
         // If the code experiences any errors it will log the error to the console.
         if (error) {
-          return console.log(error);
+            return console.log(error);
         }
-      
+
         // We will then print the contents of data
         console.log(data);
-      
-        var doThis = data;
-      
-        var heyLiri = doThis;
-      });
-      
+
+        heyLiri = data;
+    });
+
 }
 
 //TWITTER
 //activates twitter get
-else if (heyLiri === "my-tweets") {
+if (heyLiri === "my-tweets") {
 
     //I want the API to return last 20 tweets and when they were created
     var params = { screen_name: '@TonyJabroni8', count: 20, include_rts: false, exclude_replies: true };
@@ -61,51 +110,8 @@ else if (heyLiri === "my-tweets") {
 
 //OMDB================
 // Activates omdb search
-if (heyLiri === "movie-this") {
-
-    //If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
-    if (nodeArgs[3] === undefined) {
-        var movie = "Mr.+Nobody"
-    }
-
-    //looks for additional words in search term, puts + between words
-    else {
-        for (var i = 3; i < nodeArgs.length; i++) {
-
-            if (i > 3 && i < nodeArgs.length) {
-
-                var movie = movie + "+" + nodeArgs[i]
-            }
-
-            else {
-                movie += nodeArgs[i];
-            }
-        }
-    }
-
-    //the query url
-    var movieQuery = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&tomatoes=true&apikey=trilogy";
-
-    // Request module on a URL with a JSON
-    request(movieQuery, function (error, response, body) {
-        // If no errors and rxesponse code is 200 
-        if (!error && response.statusCode === 200) {
-
-            // Print out the omdb return
-            console.log("=====================");
-            console.log("Title: " + JSON.parse(body).Title);
-            console.log("Year: " + JSON.parse(body).Year);
-            console.log("iMDB Rating: " + JSON.parse(body).imdbRating);
-            console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
-            console.log("Country: " + JSON.parse(body).Country);
-            console.log("Language: " + JSON.parse(body).Language);
-            console.log("Plot: " + JSON.parse(body).Plot);
-            console.log("Actors: " + JSON.parse(body).Actors);
-        }
-        else console.log("There was an error.")
-    }
-    )
-
+else if (heyLiri === "movie-this") {
+movieSearch();
 }
 
 //SPOTIFY==============
